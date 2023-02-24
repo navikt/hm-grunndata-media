a1a72125e1f3aee2942382170dc65bed219b59d1
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-02-24 14:19:04 +0100

    added function to stream file to gcp

--- src/main/kotlin/no/nav/hm/grunndata/media/storage/StorageUpload.kt
+++ src/main/kotlin/no/nav/hm/grunndata/media/storage/StorageUpload.kt
@@ -1,28 +1,35 @@
 package no.nav.hm.grunndata.media.storage
 
-import com.google.cloud.storage.Blob
+import com.google.cloud.storage.BlobId
+import com.google.cloud.storage.BlobInfo
+import com.google.cloud.storage.Storage
+import io.micronaut.objectstorage.googlecloud.GoogleCloudStorageConfiguration
 import io.micronaut.objectstorage.googlecloud.GoogleCloudStorageOperations
-import io.micronaut.objectstorage.request.UploadRequest
-import io.micronaut.objectstorage.response.UploadResponse
 import jakarta.inject.Singleton
 import org.slf4j.LoggerFactory
 import java.net.URI
-import java.nio.file.Path
+
 
 @Singleton
-class StorageUpload(private val objectStorage: GoogleCloudStorageOperations) {
+class StorageUpload(private val objectStorage: GoogleCloudStorageOperations,
+                    private val storage: Storage,
+                    private val config: GoogleCloudStorageConfiguration
+) {
 
     companion object {
         private const val PREFIX = "grunndata/media"
         private val LOG = LoggerFactory.getLogger(StorageUpload::class.java)
     }
 
-    fun upload(uri: URI): StorageResponse {
-        LOG.info("Store $uri to gcp")
-        val objectStorageUpload: UploadRequest = UploadRequest.fromPath(Path.of(uri), PREFIX)
-        val response: UploadResponse<Blob> = objectStorage.upload(objectStorageUpload)
-        return StorageResponse(eTag = response.eTag, key = response.key, size = response.nativeResponse.size,
-            md5hash = response.nativeResponse.md5ToHexString)
+    fun uploadStream(uri: URI): StorageResponse {
+        val objectName = uri.path.substringAfterLast("/").trim()
+        val key = "$PREFIX/$objectName"
+        LOG.info("Store $key to gcp")
+        val blobId: BlobId = BlobId.of(config.bucket, key)
+        val blobInfo = BlobInfo.newBuilder(blobId).build()
+        val blob = storage.createFrom(blobInfo, uri.toURL().openStream())
+        return StorageResponse(eTag = blob.etag, key = key, size = blob.size,
+            md5hash = blob.md5ToHexString)
     }
 
 }
