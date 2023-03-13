1cba0575e014fe8ce4e594d174b9ca01069b8ca4
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-03-13 12:39:29 +0100

    fix build fail

--- src/main/kotlin/no/nav/hm/grunndata/media/AliveController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/media/AliveController.kt
@@ -2,10 +2,7 @@ package no.nav.hm.grunndata.media
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.security.annotation.Secured
-import io.micronaut.security.rules.SecurityRule
 
-@Secured(SecurityRule.IS_ANONYMOUS)
 @Controller("/internal")
 class AliveController {
 
