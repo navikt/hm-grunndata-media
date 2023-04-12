f3be63032cc961646f074e5a231d26b07593947c
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-04-12 15:14:49 +0200

    disable a test

--- src/test/kotlin/no/nav/hm/grunndata/media/storage/MediaUploadTest.kt
+++ src/test/kotlin/no/nav/hm/grunndata/media/storage/MediaUploadTest.kt
@@ -3,7 +3,6 @@ package no.nav.hm.grunndata.media.storage
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.Test
 import java.net.URI
 
 @MicronautTest
@@ -12,7 +11,7 @@ class MediaUploadTest(
     private val objectMapper: ObjectMapper
 ) {
 
-    @Test //use for testing gcp buckets
+    //@Test //use for testing gcp buckets
     fun testUploadDownload() {
         val resource = MediaUploadTest::class.java.classLoader.getResource("66131.jpg")
         val sourceUri = URI("https://www.hjelpemiddeldatabasen.no/blobs/orig/66131.jpg")
@@ -21,6 +20,6 @@ class MediaUploadTest(
 
         println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response))
         // test delete
-        gcstorageUpload.delete(destinationUri)
+        //gcstorageUpload.delete(destinationUri)
     }
 }
