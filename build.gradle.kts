87f448cb3adc8d32472ca2c94e13efb59e04be9e
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-05-16 11:24:50 +0200

    dto upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val jakartaPersistenceVersion = "3.1.0"
 val rapidsRiversVersion = "202305081226"
-val grunndataDtoVersion = "202305121616"
+val grunndataDtoVersion = "202305150838"
 val imageIOJpgPluginVersion = "3.9.4"
 
 
