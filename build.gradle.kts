aeb9d18e4dc49b1507306f619f0d86eb07f658f8
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-07-31 08:52:27 +0200

    update dto lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val jakartaPersistenceVersion = "3.1.0"
 val rapidsRiversVersion = "202305081226"
-val grunndataDtoVersion = "202307281019"
+val grunndataDtoVersion = "202307310829"
 val imageIOJpgPluginVersion = "3.9.4"
 
 
