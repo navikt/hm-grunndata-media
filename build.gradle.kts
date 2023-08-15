bb65442db3c70893adcd419385573a48d4faf83c
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-08-15 08:55:21 +0200

    upgrade rr lib and micronaut 4.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val jvmTarget = "17"
-val micronautVersion = "4.0.2"
+val micronautVersion = "4.0.3"
 val kafkaVersion = "3.2.1"
 val micrometerRegistryPrometheusVersion = "1.9.1"
 val junitJupiterVersion = "5.9.2"
@@ -13,7 +13,7 @@ val tcVersion = "1.17.6"
 val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val jakartaPersistenceVersion = "3.1.0"
-val rapidsRiversVersion = "202308011528"
+val rapidsRiversVersion = "202308150807"
 val grunndataDtoVersion = "202307310829"
 
 
