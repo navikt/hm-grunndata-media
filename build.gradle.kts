9f3c9e4b86e45fd68eace8ffc6664255ef20cdb7
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-09-12 15:35:23 +0200

    upgrade micronaut 4.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val jvmTarget = "17"
-val micronautVersion = "4.0.3"
+val micronautVersion = "4.0.5"
 val kafkaVersion = "3.2.1"
 val micrometerRegistryPrometheusVersion = "1.9.1"
 val junitJupiterVersion = "5.9.2"
@@ -14,7 +14,7 @@ val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val jakartaPersistenceVersion = "3.1.0"
 val rapidsRiversVersion = "202309071223"
-val grunndataDtoVersion = "202309071203"
+val grunndataDtoVersion = "202309071443"
 
 
 group = "no.nav.hm"
