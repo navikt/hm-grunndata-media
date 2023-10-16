8fe7676b7824f041d3bd3168b67fafa1e9bb5621
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-10-16 12:16:27 +0200

    upgrade to micronaut 4.1.4, and dto lib, fix compatbleWith

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val jvmTarget = "17"
-val micronautVersion = "4.1.0"
+val micronautVersion = "4.1.4"
 val kafkaVersion = "3.2.1"
 val micrometerRegistryPrometheusVersion = "1.9.1"
 val junitJupiterVersion = "5.9.2"
@@ -14,7 +14,7 @@ val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val jakartaPersistenceVersion = "3.1.0"
 val rapidsRiversVersion = "202309071223"
-val grunndataDtoVersion = "202309290954"
+val grunndataDtoVersion = "202310121606"
 
 
 group = "no.nav.hm"
@@ -25,7 +25,7 @@ plugins {
     kotlin("kapt") version "1.8.22"
     id("java")
     id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("io.micronaut.application") version "4.1.0"
+    id("io.micronaut.application") version "4.1.1"
 }
 
 configurations.all {
