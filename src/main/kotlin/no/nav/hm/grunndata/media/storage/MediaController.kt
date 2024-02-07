0c02fb351444f7aab119ef8dcfb9f9a148d3473b
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-02-07 12:19:36 +0100

    Lagre med filename og ikkje name som bare er "file"

--- src/main/kotlin/no/nav/hm/grunndata/media/storage/MediaController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/media/storage/MediaController.kt
@@ -5,9 +5,14 @@ import io.micronaut.http.annotation.Delete
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.multipart.CompletedFileUpload
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.reactive.asFlow
-import no.nav.hm.grunndata.media.model.*
+import no.nav.hm.grunndata.media.model.Media
+import no.nav.hm.grunndata.media.model.MediaDTO
+import no.nav.hm.grunndata.media.model.MediaRepository
+import no.nav.hm.grunndata.media.model.MediaStatus
+import no.nav.hm.grunndata.media.model.toDTO
 import no.nav.hm.grunndata.media.storage.UploadMediaController.Companion.V1_UPLOAD_MEDIA
 import no.nav.hm.grunndata.media.sync.BadRequestException
 import no.nav.hm.grunndata.media.sync.UknownMediaSource
@@ -17,22 +22,24 @@ import org.reactivestreams.Publisher
 import org.slf4j.LoggerFactory
 import java.net.URI
 import java.time.LocalDateTime
-import java.util.*
+import java.util.UUID
 
 @Controller(V1_UPLOAD_MEDIA)
-class UploadMediaController(private val storageService: StorageService,
-                            private val mediaRepository: MediaRepository,
-                            private val mediaStorageConfig: MediaStorageConfig) {
+class UploadMediaController(
+    private val storageService: StorageService,
+    private val mediaRepository: MediaRepository,
+    private val mediaStorageConfig: MediaStorageConfig
+) {
 
     companion object {
         const val V1_UPLOAD_MEDIA = "/api/v1/upload/media"
         const val REGISTER_UPLOAD_PREFIX = "register"
-        const val IMPORT_UPLOAD_PREFIX="import"
+        const val IMPORT_UPLOAD_PREFIX = "import"
         private val LOG = LoggerFactory.getLogger(UploadMediaController::class.java)
     }
 
     @Get("/{app}/oid/{oid}")
-    suspend fun getMediaByOid(app: String, oid: UUID): List<MediaDTO>  {
+    suspend fun getMediaByOid(app: String, oid: UUID): List<MediaDTO> {
         LOG.info("Got request for media list of oid $oid from $app")
         return mediaRepository.findByOid(oid).map { it.toDTO() }
     }
@@ -42,13 +49,17 @@ class UploadMediaController(private val storageService: StorageService,
         consumes = [io.micronaut.http.MediaType.MULTIPART_FORM_DATA],
         produces = [io.micronaut.http.MediaType.APPLICATION_JSON]
     )
-    suspend fun uploadFile(oid: UUID, app: String,
-                           file: CompletedFileUpload): MediaDTO {
+    suspend fun uploadFile(
+        oid: UUID, app: String,
+        file: CompletedFileUpload
+    ): MediaDTO {
         return uploadToStorage(file, app, oid)
     }
 
-    private suspend fun uploadToStorage(file: CompletedFileUpload, app:String,
-                                        oid: UUID): MediaDTO {
+    private suspend fun uploadToStorage(
+        file: CompletedFileUpload, app: String,
+        oid: UUID
+    ): MediaDTO {
         LOG.info("Got request to upload file for ${file.name} oid: $oid and $app")
         val type = getMediaType(file)
         if (type == MediaType.OTHER) throw UknownMediaSource("only png, jpg, pdf is supported")
@@ -63,7 +74,7 @@ class UploadMediaController(private val storageService: StorageService,
             Media(
                 id = id,
                 oid = oid,
-                filename = file.name,
+                filename = file.filename,
                 uri = uri,
                 sourceUri = "${mediaStorageConfig.cdnurl}/$uri",
                 type = type,
@@ -76,7 +87,7 @@ class UploadMediaController(private val storageService: StorageService,
     }
 
     private fun getMediaExtension(extension: String): String {
-        return when(extension.lowercase()) {
+        return when (extension.lowercase()) {
             "jpg", "jpeg" -> "jpg"
             "png" -> "png"
             "pdf" -> "pdf"
@@ -89,13 +100,15 @@ class UploadMediaController(private val storageService: StorageService,
         consumes = [io.micronaut.http.MediaType.MULTIPART_FORM_DATA],
         produces = [io.micronaut.http.MediaType.APPLICATION_JSON]
     )
-    suspend fun uploadFiles(oid: UUID, app: String,
-                            files: Publisher<CompletedFileUpload>): List<MediaDTO> =
+    suspend fun uploadFiles(
+        oid: UUID, app: String,
+        files: Publisher<CompletedFileUpload>
+    ): List<MediaDTO> =
         files.asFlow().map { uploadToStorage(it, app, oid) }.toList()
 
 
     @Delete("/{app}/{oid}/{uri}")
-    suspend fun deleteByOidAndUri(app: String, oid:UUID, uri: String): MediaDTO =
+    suspend fun deleteByOidAndUri(app: String, oid: UUID, uri: String): MediaDTO =
         mediaRepository.findByOidAndUri(oid, uri)?.let {
             LOG.info("Got deleted request from $app for $oid and $uri")
             mediaRepository.update(it.copy(status = MediaStatus.DELETED, updated = LocalDateTime.now())).toDTO()
