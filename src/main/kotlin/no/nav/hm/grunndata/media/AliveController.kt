1a1367d3820ca631d996a8e562ca97df71f3dc8b
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-05-23 12:18:05 +0200

    restart if consumer is down

--- src/main/kotlin/no/nav/hm/grunndata/media/AliveController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/media/AliveController.kt
@@ -3,19 +3,25 @@ package no.nav.hm.grunndata.media
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
+import no.nav.helse.rapids_rivers.KafkaRapid
 import no.nav.hm.grunndata.media.storage.StorageService
 import org.slf4j.LoggerFactory
 import java.net.URI
 
 @Controller("/internal")
-class AliveController(private val storageService: StorageService) {
+class AliveController(private val storageService: StorageService, private val kafkaRapid: KafkaRapid) {
 
     companion object {
         private val LOG = LoggerFactory.getLogger(AliveController::class.java)
     }
 
     @Get("/isAlive")
-    fun alive() = "ALIVE"
+    fun alive(): String {
+        if (kafkaRapid.isConsumerClosed()) {
+            throw Exception("Error: consumer is closed")
+        }
+        return "ALIVE"
+    }
 
     @Get("/isReady")
     fun ready() = "OK"
