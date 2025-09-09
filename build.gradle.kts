e870a6a544bc4e8607e4ea441efdb10ce7b4570f
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-09-09 08:01:57 +0200

    upgrade micronaut to v4.9.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val jvmTarget = "17"
-val micronautVersion = "4.9.1"
+val micronautVersion = "4.9.3"
 val junitJupiterVersion = "5.9.2"
 val logbackClassicVersion = "1.4.12"
 val logbackEncoderVersion = "7.3"
